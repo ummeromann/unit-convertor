99e513622289ddc3a1ce31d175b89c23ade911e9
ummeromann <168990125+ummeromann@users.noreply.github.com>
2025-04-04 18:39:47 +0500

    Add files via upload

---
+++ unit-convertor.py
@@ -0,0 +1,51 @@
+import streamlit as st 
+
+st.title("🌎Unit Convertor App")
+st.markdown("### Converts Length, Weight And Time Instantly")
+st.write("Welcome! Select a category, enter a value and get the converted result in real-time")
+
+category = st.selectbox("Choose a category", ["Length", "Weight", "Time"])
+
+def convert_units(category, value, unit):
+    if category == "Length":
+        if unit == "Kilometers to Miles":
+            return value * 0.621371
+        elif unit == "Miles to Kilometers":
+            return value / 0.621371
+        
+    elif category == "Weight":
+        if unit == "Kilograms to pounds":
+            return value * 2.20462
+        elif unit == "Pounds to kilograms":
+            return value / 2.20462
+    elif category == "Time":
+        if unit == "Seconds to minutes":
+            return value / 60 
+        elif unit == "Minutes to seconds":
+            return value * 60   
+        elif unit == "Minutes to hours":
+            return value / 60
+        elif unit == "Hours to minutes":
+            return value * 60
+        elif unit == "Hours to days":
+            return value / 24
+        elif unit == "Days to hours":
+            return value * 24
+
+if category == "Length":
+    unit = st.selectbox("📏 Select Conversation", 
+    ["Miles to Kilometers", "Kilometers to Miles"])  
+elif category == "Weight":
+      unit = st.selectbox("♎ scale Select Conversation", ["Kilograms to pounds", "Pounds to kilograms"])
+
+elif category == "Time":
+      unit = st.selectbox("⌚ Select Conversation", ["Seconds to minutes", "Minutes to seconds", "Minutes to hours", "Hours to minutes", "Hours to days", "Days to hours"])   
+
+value = st.number_input("Enter the value to convert")
+
+if st.button("Convert"):
+    result = convert_units(category, value, unit)
+    st.success(f"The result is {result:.2f}")
+
+
+
